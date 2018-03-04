410248b7e7f67577c7e2ddd940a717643c049eee
Slyther <joseph.a.griff@gmail.com>
2018-03-03 21:54:29 -0600

    Testing basic layout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            startDate: moment(),
+            daysCount: 0,
+            countryCode: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    render() {
+        return (
+                <div className="doc row">
+                    <div className="col-xs-12 row container">
+                        Testing!
+                    </div>
+                </div>
+            );
+    }
 }
 
 export default App;
