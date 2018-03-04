2ce5d0372fbc5bb3c943263c2d86a438d9d4add4
Slyther <joseph.a.griff@gmail.com>
2018-03-03 22:31:06 -0600

    Error message, setup for showing calendar, onSubmit conditions with countryCode.

--- src/App.js
+++ src/App.js
@@ -12,10 +12,18 @@ class App extends Component {
             startDate: moment(),
             daysCount: 0,
             countryCode: '',
+            showError: false,
+            showCalendars: false,
+            calendarsJSX: '',
+            error: '',
+        }
+        this.internals = {
+            countryCodes: ['AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AN', 'AO', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ', 'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CX', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ', 'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FK', 'FM', 'FO', 'FR', 'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY', 'HK', 'HM', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ', 'OM', 'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY', 'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SY', 'SZ', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ', 'UA', 'UG', 'UM', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU', 'WF', 'WS', 'YE', 'YT', 'ZA', 'ZM', 'ZW']
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        this.renderCalendars = this.renderCalendars.bind(this);
     }
 
     handleChange(event) {
@@ -31,8 +39,24 @@ class App extends Component {
         });
     }
 
-    onSubmit() {
-        console.log("Testing!");
+    onSubmit(){
+        const { startDate, daysCount, countryCode } = this.state;
+        if(!this.internals.countryCodes.includes(countryCode)){
+            this.setState({showCalendars: false, showError: true, error: 'Please select a valid country code!'});
+            return;
+        }else if(this.state.daysCount == 0) {
+            this.setState({showCalendars: false, showError: true, error: 'Zero number of days!'});
+            return;
+        }else if(this.state.showError == true) {
+            this.setState({showError: false});
+        }
+        this.renderCalendars(moment(startDate), daysCount, countryCode);
+    }
+
+    renderCalendars(startDate, daysCount, countryCode){
+        console.log(moment(startDate));
+        console.log(daysCount);
+        console.log(countryCode);
     }
 
     render() {
@@ -77,6 +101,18 @@ class App extends Component {
                         />
                     </div>
                 </div>
+                {
+                    this.state.showError &&
+                    <div className="error col-xs-12">
+                        <div className="container">
+                            {this.state.error}
+                        </div>
+                    </div>
+                }
+                {
+                    this.state.showCalendars &&
+                    this.state.calendarsJSX
+                }
             </div>
         );
     }
